ebadc5b8c5ace041c2505004cf67fca628f7b4b8
LifeIsTech <funkinaoki@gmail.com>
2021-02-02 22:22:29 +0900

    finish man

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -11,20 +11,57 @@ class ViewController: UIViewController {
     
     //画像表示変数
     @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var profileLabel: UILabel!
+    
+    
     //ラベル表示変数
     @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profileImageView.isHidden = true
-        
-        profileCommentLabel.isHidden = true
+//        profileImageView.isHidden = true
+//
+//        profileCommentLabel.isHidden = true
     }
     @IBAction func  tapButton1(){
-        profileImageView.isHidden = false
+        profileImageView.image = UIImage(named: "philImage")
+        profileLabel.text = "名前"
         
-        profileCommentLabel.isHidden = false
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
+//        profileImageView.isHidden = false
+//
+//        profileCommentLabel.isHidden = false
+    }
+    @IBAction func  tapButton2(){
+        profileImageView.image = UIImage(named: "trackImage")
+        profileLabel.text = "スポーツ"
+        
+        profileCommentLabel.text = "走れ走れだうおおおおお"
+//        profileImageView.isHidden = false
+//
+//        profileCommentLabel.isHidden = false
+    }
+    
+    @IBAction func  tapButton3(){
+        profileImageView.image = UIImage(named: "appleImage")
+        profileLabel.text = "ああ"
+        
+        profileCommentLabel.text = "iPkdfjごsglだsjよ"
+//        profileImageView.isHidden = false
+//
+//        profileCommentLabel.isHidden = false
+    }
+    
+    @IBAction func  tapButton4(){
+        profileImageView.image = UIImage(named: "flightImage")
+        profileLabel.text = "いいいい"
+        
+        profileCommentLabel.text = "あはははははあは"
+//        profileImageView.isHidden = false
+//
+//        profileCommentLabel.isHidden = false
     }
 
 
